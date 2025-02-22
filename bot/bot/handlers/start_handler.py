fc5d2d2bc916495e4f61468fd6828a4de89ffed5
c0ral0vii <demidovic.pavel2@gmail.com>
2025-02-22 16:30:42 +0300

    first commit

---
+++ bot/bot/handlers/start_handler.py
@@ -0,0 +1,11 @@
+from aiogram import Router, types
+from aiogram.filters import CommandStart
+
+router = Router()
+
+START_MESSAGE = "Отправьте ссылку на домен который необходимо отслеживать"
+
+
+@router.message(CommandStart)
+async def start_handler(message: types.Message):
+    await message.answer("")
